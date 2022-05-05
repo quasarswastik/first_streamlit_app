99d57e6efa051103615fdf70770cacbcb8d74386
quasarswastik <quasarswastik@gmail.com>
2022-05-05 09:59:19 +0800

    Update streamlit_app.py
    try...except

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,17 +23,24 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
+try:
+  fruit_choice = streamlit.text_input("What fruit would you like information about?")
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalised)
+except URLError as e:
+  streamlit.error()
 
-fruit_choice = streamlit.text_input("What fruit would you like information about?", "Kiwi")
-streamlit.write('The user entered', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json())
 
 # normalises json
-fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
+# fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
 #present as output
-streamlit.dataframe(fruityvice_normalised)
+# streamlit.dataframe(fruityvice_normalised)
 
 streamlit.stop()
 
